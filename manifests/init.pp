5590b0989191c2ff5dda50151abb7342db027d1d
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2015-10-26 16:03:49 +0100

    Add headers

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,25 @@
 #
 # === Parameters
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#
+# [*package_name*] - Specify gitlab multi runner package name
+#                   Defaults to gitlab-ci-multi-runner
+# [*service_name*] - Specify the service name for gitlab multi runner
+#                   Defaults to gitlab-runner
+# [*manage_package_repo*] - Manage package repository for multi runner
+#                   Defaults to undef
+# [*user*] - Default user for multi runner
+#             Defaults to gitlab-runner
+# [*group*] - Default group for multi runner
+#             Defaults to gitlab-runner
+# [*concurrent*] - Number of concurrents jobs
+#             Defaults to 1 
 class gitlab_ci_multi_runner (
-  $package_name = $gitlab_ci_multi_runner::params::package_name,
-  $service_name = $gitlab_ci_multi_runner::params::service_name,
+  $package_name        = $gitlab_ci_multi_runner::params::package_name,
+  $service_name        = $gitlab_ci_multi_runner::params::service_name,
+  $manage_package_repo = $gitlab_ci_multi_runner::params::manage_package_repo,
+  $user                = $gitlab_ci_multi_runner::params::user,
+  $group               = $gitlab_ci_multi_runner::params::group,
+  $concurrent          = $gitlab_ci_multi_runner::params::concurrent,
 ) inherits gitlab_ci_multi_runner::params {
 
   # validate parameters here
