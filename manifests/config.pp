5e8435d548be022c83fa9c1895ac294ed700e1bb
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2015-10-26 16:03:40 +0100

    Add config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,5 +2,16 @@
 #
 # This class is called from gitlab_ci_multi_runner
 #
-class gitlab_ci_multi_runner::config {
+class gitlab_ci_multi_runner::config (
+  $concurrent = $gitlab_ci_multi_runner::concurrent,
+) {
+  
+  file { $::gitlab_ci_multi_runner::config_file:
+    ensure  => present,
+    owner   => $::gitlab_ci_multi_runner::user,
+    group   => $::gitlab_ci_multi_runner::group,
+    content => template('gitlab_ci_multi_runner/config.toml.erb'),
+    require => Package[$gitlab_ci_multi_runner::package_name],
+    notify  => Service[$gitlab_ci_multi_runner::service_name],
+  }
 }
