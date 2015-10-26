6f8128c8c4bc95b14650b32002f40a1c64b7b4d7
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2015-10-26 16:04:05 +0100

    Add repo and https

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,13 @@
 #
 class gitlab_ci_multi_runner::install {
 
+  ensure_packages('apt-transport-https')
+
+  if($::gitlab_ci_multi_runner::manage_package_repo) {
+    class { 'gitlab_ci_multi_runner::repo':
+    }
+  }
+
   package { $gitlab_ci_multi_runner::package_name:
     ensure => present,
   }
